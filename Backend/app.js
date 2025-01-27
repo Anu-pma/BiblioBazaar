0adcec5f16b5088467e4fa439d8302b1113640c4
Anupma Kumari <anupma30102005@gmail.com>
2025-01-28 02:37:39 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,5 @@
 const express = require("express"); //req express in a var
 const app = express(); //now app can use all functionalities of exp
-
-// const mongoose = require('mongoose');
-// mongoose.connect("mongodb+srv://Anupma-1111:@bookcluster.s50ir.mongodb.net/?retryWrites=true&w=majority&appName=BookCluster");
-// const User = require('./models/userModel');
-
 require("dotenv").config(); //requiring configfile of .env
 require("./connec/conn");
 const User=require("./routes/user")
@@ -14,9 +9,6 @@ const Cart = require("./routes/cart")
 const Order = require("./routes/order")
 app.use(express.json()); //data is coming in json format
 //routes
-
-//app.get("/", (req, res) => res.send("Server is up and running"));
-
 app.use("/api/v1",User);
 app.use("/api/v1",Books);
 app.use("/api/v1",Favourite);
